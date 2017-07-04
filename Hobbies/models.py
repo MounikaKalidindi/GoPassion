217b397d7e2d7a105188bf52e5593b05ef5747f6
ayshwaryasree <ayshwaryasree.ayshu>
2017-07-04 10:13:05 +0530

    form for poost

--- Hobbies/models.py
+++ Hobbies/models.py
@@ -4,79 +4,104 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 # Create your models here.
-class Profile(models.Model) :
-	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-	intrest = models.CharField(max_length=50)
-	name = models.CharField(max_length=50)
-@receiver (post_save, sender=User)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    intrest = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-	if created :
-		Profile.objects.create(user=instance)
-@receiver (post_save, sender=User)
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-	instance.profile.save()
+    instance.profile.save()
+
 
 class Categories(models.Model):
-	category_name = models.CharField(max_length=50)
-	def __str__(self):
-		return self.category_name
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
 
 class Sub_Categories(models.Model):
-	sub_category_name = models.CharField(max_length=50)
-	def __str__(self):
-                return self.sub_category_name
+    sub_category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sub_category_name
+
 
 class Categories_Sub_Categories_Mapping(models.Model):
-	category_id = models.ForeignKey(Categories, on_delete = models.CASCADE)
-	sub_category_id = models.ForeignKey(Sub_Categories, on_delete = models.CASCADE)
-	def __str__(self):
-		return "{0}-{1}".format(self.category_id, self.sub_category_id)
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    sub_category_id = models.ForeignKey(Sub_Categories, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}-{1}".format(self.category_id, self.sub_category_id)
+
 
 class Sub_Categories1(models.Model):
-	#sub_category = models.ForeignKey(Categories_Sub_Categories_Mapping, on_delete = models.CASCADE)
-	sub_category1_name = models.CharField(max_length=50)
-	def __str__(self):
-		return  self.sub_category1_name
+    # sub_category = models.ForeignKey(Categories_Sub_Categories_Mapping, on_delete = models.CASCADE)
+    sub_category1_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sub_category1_name
+
 
 class Sub_Categories_Sub_Categories1_Mapping(models.Model):
-	sub_category_id = models.ForeignKey(Sub_Categories, on_delete = models.CASCADE)
-	sub_category1_id = models.ForeignKey(Sub_Categories1, on_delete = models.CASCADE)
-	def __str__(self):
-		return "{0}-{1}".format(self.sub_category_id, self.sub_category1_id)
+    sub_category_id = models.ForeignKey(Sub_Categories, on_delete=models.CASCADE)
+    sub_category1_id = models.ForeignKey(Sub_Categories1, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}-{1}".format(self.sub_category_id, self.sub_category1_id)
+
 
 class Tutorials(models.Model):
-	sub_category1_map_id = models.ForeignKey(Sub_Categories_Sub_Categories1_Mapping, on_delete = models.CASCADE)
-	description = models.CharField(max_length=500)
-	video = models.CharField(max_length=300)
-	image = models.CharField(max_length=300)
-	def __str__(self):
-		return "{0}-{1}-{2}-{3}".format(self.sub_category1_map_id, self.description, self.video, self.image)
+    sub_category1_map_id = models.ForeignKey(Sub_Categories_Sub_Categories1_Mapping, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    video = models.CharField(max_length=300)
+    image = models.CharField(max_length=300)
+
+    def __str__(self):
+        return "{0}-{1}-{2}-{3}".format(self.sub_category1_map_id, self.description, self.video, self.image)
+
 
 class FeedBack(models.Model):
-	feedback = models.CharField(max_length=50)
-	suggesions = models.CharField(max_length=50)
-	category_map_id = models.ForeignKey(Categories_Sub_Categories_Mapping, on_delete = models.CASCADE)
-	def __str__(self):
-		return "{0}-{1}-{2}".format(self.feedback, self.suggesions, self.category_id)
+    feedback = models.CharField(max_length=50)
+    suggesions = models.CharField(max_length=50)
+    category_map_id = models.ForeignKey(Categories_Sub_Categories_Mapping, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.feedback, self.suggesions, self.category_id)
+
 
 class Posts(models.Model):
-	user_id = models.ForeignKey(User, on_delete = models.CASCADE)
-	category_map_id = models.ForeignKey(Categories_Sub_Categories_Mapping, on_delete = models.CASCADE)
-	post_adv = models.BooleanField()
-	src = models.CharField(max_length=300)
-	likes = models.IntegerField()
-	description = models.CharField(max_length=300)
-	
-	def __str__(self):
-		return "{0}-{1}-{2}-{3}-{4}-{5}".format(self.user_id, self.category_map_id.id, self.post_adv, self.src, self.likes, self.description)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_map_id = models.ForeignKey(Categories_Sub_Categories_Mapping, on_delete=models.CASCADE)
+    post_adv = models.BooleanField()
+    src = models.CharField(max_length=300)
+    likes = models.IntegerField()
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return "{0}-{1}-{2}-{3}-{4}-{5}".format(self.user_id, self.category_map_id.id, self.post_adv, self.src,
+                                                self.likes, self.description)
+
 
 class Advertisements(models.Model):
-	user_id = models.ForeignKey(User)
-	category_map_id = models.ForeignKey(Categories_Sub_Categories_Mapping, on_delete = models.CASCADE)
-	post_id = models.ForeignKey(Posts)
-	def __str__(self):
-		return "{0}-{1}-{2}".format(self.user_id, self.category_id, self.post_id)
+    user_id = models.ForeignKey(User)
+    category_map_id = models.ForeignKey(Categories_Sub_Categories_Mapping, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Posts)
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.user_id, self.category_id, self.post_id)
+
+
 """
 class Utilities(models.Model):
 	utility = models.CharField(max_length=50)
@@ -90,15 +115,20 @@ class Details_Utilities_Mapping(models.Model):
 	def __str__(self):
 		return "{0}-{1}".format(self.details_id, self.utility_id)
 """
-class Followers(models.Model):
-	user_id = models.ForeignKey(User, related_name="user_id", on_delete = models.CASCADE)
-	follower_id = models.ForeignKey(User, related_name="follower_id", on_delete = models.CASCADE)
-	def __str__(self):
-		return "{0}-{1}".format(self.user_id, self.follower_id)
-class ScoreBoard(models.Model):
-	category_id = models.ForeignKey(Categories, on_delete = models.CASCADE)
-	rank = models.IntegerField()
-	user_name = models.ForeignKey(Profile, on_delete = models.CASCADE)
-	def __str__(self):
-		return "{0}-{1}-{2}".format(self.category_id, self.rank, self.user_name)
 
+
+class Followers(models.Model):
+    user_id = models.ForeignKey(User, related_name="user_id", on_delete=models.CASCADE)
+    follower_id = models.ForeignKey(User, related_name="follower_id", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}-{1}".format(self.user_id, self.follower_id)
+
+
+class ScoreBoard(models.Model):
+    category_id = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    rank = models.IntegerField()
+    user_name = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.category_id, self.rank, self.user_name)
--- Hobbies/views.py
+++ Hobbies/views.py
@@ -1,71 +1,75 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
-#from library.models import Myclass
-from Hobbies.models import *
+# from library.models import Myclass
+from models import *
 from django.views import generic
 from django.template import loader
+from forms import *
 
 # Create your views here.
-def index(request) :
-	#templates = loader.get_template("Hobbies/category_list.html")
-	#return HttpResponse(templates.render())
-	return HttpResponse( "Mouniii")
+def index(request):
+    # templates = loader.get_template("Hobbies/category_list.html")
+    # return HttpResponse(templates.render())
+    return HttpResponse("Mouniii")
 
 
-def categories_list(request):	
-	#cat = Categories.objects.all().order_by('id')[:3]
-	category_obj = get_list_or_404(Categories)
-	category_map_obj = get_list_or_404(Categories_Sub_Categories_Mapping)
-	sub_category_obj = get_list_or_404(Sub_Categories)
-	sub_category_map_obj = get_list_or_404(Sub_Categories_Sub_Categories1_Mapping)
-	#c_id = getCategoryId(cat_name)
-	#sub_c_obj = get_list_or_404(Categories_Sub_Categories_Mapping, category_id = c_id)
-	return render(
-		request,
-		'Hobbies/category_list.html',
-		context = {'categories':category_obj, 'categories_map':category_map_obj, 'sub_categories':sub_category_obj, 'sub_categories_map':sub_category_map_obj}
-	)
+def categories_list(request):
+    # cat = Categories.objects.all().order_by('id')[:3]
+    category_obj = get_list_or_404(Categories)
+    category_map_obj = get_list_or_404(Categories_Sub_Categories_Mapping)
+    sub_category_obj = get_list_or_404(Sub_Categories)
+    sub_category_map_obj = get_list_or_404(Sub_Categories_Sub_Categories1_Mapping)
+    # c_id = getCategoryId(cat_name)
+    # sub_c_obj = get_list_or_404(Categories_Sub_Categories_Mapping, category_id = c_id)
+    return render(
+        request,
+        'Hobbies/category_list.html',
+        context={'categories': category_obj, 'categories_map': category_map_obj, 'sub_categories': sub_category_obj,
+                 'sub_categories_map': sub_category_map_obj}
+    )
+
 
 def getCategoryId(cat_name):
-	#c_obj = get_list_or_404(Categories)
-	c_id = get_object_or_404 (Categories, category = cat_name)
-	return (c_id.id)
+    # c_obj = get_list_or_404(Categories)
+    c_id = get_object_or_404(Categories, category=cat_name)
+    return (c_id.id)
+
 
 def nav_cat_list(request):
-	category_obj = get_list_or_404(Categories)
-	category_map_obj = get_list_or_404(Categories_Sub_Categories_Mapping)
-	sub_category_obj = get_list_or_404(Sub_Categories)
-	return render(
-		request,
-		'Hobbies/cat_list.html',
-		context = {'categories': category_obj, 'categories_map': category_map_obj, 'sub_categories':sub_category_obj}
-		)	
+    category_obj = get_list_or_404(Categories)
+    category_map_obj = get_list_or_404(Categories_Sub_Categories_Mapping)
+    sub_category_obj = get_list_or_404(Sub_Categories)
+    return render(
+        request,
+        'Hobbies/cat_list.html',
+        context={'categories': category_obj, 'categories_map': category_map_obj, 'sub_categories': sub_category_obj}
+    )
+
+
 def getDifferentOptions(request, category_name, sub_category_name):
-	return render(
-		request,
-		'Hobbies/options.html',
-		context = {'category_name': category_name, 'sub_category_name' : sub_category_name}
-		)
+    return render(
+        request,
+        'Hobbies/options.html',
+        context={'category_name': category_name, 'sub_category_name': sub_category_name}
+    )
+
 
 def getDifferentOptions1(request):
-	category_obj = get_list_or_404(Categories)
-	category_map_obj = get_list_or_404(Categories_Sub_Categories_Mapping)
-#	sub_category_obj = get_list_or_404(Sub_Categories)
-	posts_obj = get_list_or_404(Posts)
-	return render(
-		request,
-		'Hobbies/options.html',
-		context = {'categories': category_obj, 'categories_map': category_map_obj, 'posts':posts_obj}
-		)
-"""
-class CategoriesListView(generic.ListView):
-	model = Categories
+    category_obj = get_list_or_404(Categories)
+    category_map_obj = get_list_or_404(Categories_Sub_Categories_Mapping)
+    #	sub_category_obj = get_list_or_404(Sub_Categories)
+    posts_obj = get_list_or_404(Posts)
+    return render(
+        request,
+        'Hobbies/options.html',
+        context={'categories': category_obj, 'categories_map': category_map_obj, 'posts': posts_obj}
+    )
 
-	def get_context_data(self):
-		context = super(CategoriesListView, self).get_context_data()
-		return context
-		
-"""
-
-		
-
+def addpost(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			return HttpResponse("<h1>thanks for the post</h1>")
+	else:
+		form = PostForm()
+	return render(request, 'Hobbies/form.html', {'form':form, 'form_name': 'ADD POST FORM'})
